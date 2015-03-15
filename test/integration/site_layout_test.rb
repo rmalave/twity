0ec2dda89ceaadaf61bf4a201c6841443b054664
Rafael Malave <rafomalave@gmail.com>
2015-03-15 01:03:46 -0700

    setup some tests

--- test/integration/site_layout_test.rb
+++ test/integration/site_layout_test.rb
@@ -4,6 +4,8 @@ class SiteLayoutTest < ActionDispatch::IntegrationTest
     
     test "layout links" do
         get root_path
+        get signup_path 
+        assert_select "title", full_title("Sign up")
         assert_template 'static_pages/home'
         assert_select "a[href=?]", root_path, count: 2
         assert_select "a[href=?]", help_path
